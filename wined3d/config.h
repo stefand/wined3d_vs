22c8f0f7460722ee7daabe73e06d2e3e8473bcbe
Stefan Dösinger <stefan@codeweavers.com>
2011-07-07 22:49:52 +0200

    Initial commit

---
+++ wined3d/config.h
@@ -0,0 +1,21 @@
+#define __WINE_CONFIG_H
+
+#define HAVE_STRERROR
+#define HAVE_MEMMOVE
+#define HAVE_FLOAT_H
+
+#undef inline
+#define inline __inline
+
+#define HAVE_STRING_H
+#define HAVE_SPAWNVP
+#define HAVE_IO_H
+#define HAVE_POLL
+#define HAVE_PROCESS_H
+#define DECLSPEC_HOTPATCH
+#define HAVE__ISNAN 1
+#define HAVE__FINITE 1
+#define HAVE__STRICMP 1
+
+#include <direct.h>
+#define snprintf _snprintf
