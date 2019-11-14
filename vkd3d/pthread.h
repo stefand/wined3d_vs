b757b2ed0147a75dd5271758d1927ff1a5d787e7
Stefan Dösinger <stefan@codeweavers.com>
2019-11-14 13:33:41 +0100

    Wrap thread creation / joining.

--- vkd3d/pthread.h
+++ vkd3d/pthread.h
@@ -2,19 +2,52 @@
 
 typedef HANDLE pthread_t, pthread_mutex_t, pthread_cond_t;
 
-static inline int pthread_join(pthread_t thread, void** retval)
+static inline int pthread_join(pthread_t thread, void **retval)
 {
-	WaitForSingleObject(thread, INFINITE);
-	return 0;
+    assert(!retval);
+	return WaitForSingleObject(thread, INFINITE);
+}
+
+struct thread_wrapper
+{
+	void *(*start_routine)(void*);
+    void *arg;
+};
+
+static DWORD WINAPI thread_start(void *arg)
+{
+	struct thread_wrapper* wrap = arg;
+    void *(*start_routine)(void *) = wrap->start_routine;
+    void *orig_arg = wrap->arg;
+
+    HeapFree(GetProcessHeap(), 0, wrap);
+
+    return (ULONG_PTR)start_routine(orig_arg);
 }
 
 static inline int pthread_create(pthread_t *thread, const void *attr,
-		void * (*start_routine)(void *), void *arg)
+		void *(*start_routine)(void *), void *arg)
 {
 	DWORD tid;
-	/* FIXME: Calling convention, retval */
-	*thread = CreateThread(NULL, 0, (void *)start_routine, arg, 0, &tid);
-	return tid;
+	HANDLE t;
+	struct thread_wrapper *wrap = HeapAlloc(GetProcessHeap(), 0, sizeof(*wrap));
+
+    assert(!attr);
+
+	if (!wrap)
+		return -1;
+	wrap->start_routine = start_routine;
+	wrap->arg = arg;
+
+	t = CreateThread(NULL, 0, thread_start, &wrap, 0, &tid);
+	if (!t)
+	{
+		HeapFree(GetProcessHeap(), 0, wrap);
+		return -1;
+	}
+
+	*thread = t;
+	return 0;
 }
 
 static inline int pthread_mutex_destroy(pthread_mutex_t *mutex)
