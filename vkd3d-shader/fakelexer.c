a6e99506114d682b6cb9844a8eae7b6e8f5aaa8b
Stefan Dösinger <stefan@codeweavers.com>
2023-08-13 13:47:47 +0300

    Fix up some vkd3d updates

--- vkd3d-shader/fakelexer.c
+++ vkd3d-shader/fakelexer.c
@@ -13,3 +13,9 @@ int preproc_lexer_parse(const struct vkd3d_shader_compile_info *compile_info,
 {
     return -1;
 }
+
+struct hlsl_ir_node *hlsl_add_load_component(struct hlsl_ctx *ctx, struct list *instrs,
+    struct hlsl_ir_node *var_instr, unsigned int comp, const struct vkd3d_shader_location *loc)
+{
+    return (void *)0;
+}
